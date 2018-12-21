39230f16583932263ca35671b094857982269209
Oskar Damkjaer <oskar.damkjaer@gmail.com>
2018-12-21 09:00:24 +0100

    2.3

--- e2dot3.scala
+++ e2dot3.scala
@@ -1,34 +1,5 @@
 #!/usr/bin/env amm
 
-
-def sorted[A] (array: Array[A], ordered: (A,A) => Boolean): Boolean = {
-  def go(index: Int): Boolean = 
-    index == array.length - 1 ||
-    ordered(array(index), array(index + 1)) &&
-    go(index + 1)
- 
-  go(0)
-}
-
-val intComparer = (a:Int, b:Int) => a < b
-println(
-  sorted(
-    Array(234, 213, 2, 435), 
-    intComparer
-  )
-)
-println(
-  sorted(
-    Array(1, 3, 5, 7, 9, 20),
-    intComparer
-  )
-)
-
-val backwardsCharArray = Array('d', 'c', 'b', 'a')
-val zToAComparer = (a:Char, b:Char) => a > b
-println(
-  sorted(
-    backwardsCharArray,
-    zToAComparer
-  )
-)
+// The book said the only thing to compile would be the right answer ;) 
+def curry[A, B, C] (f: (A, B) => C): A => (B => C) = 
+  (a: A) => (b: B) => f(a, b)
